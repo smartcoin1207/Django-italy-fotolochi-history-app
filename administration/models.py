778818b9d4e809890cf165776ce6bcafabcc4807
smartcoin1207 <jackle.some99@gmail.com>
2018-08-20 15:54:56 +0200

    reorganize relations

--- administration/models.py
+++ administration/models.py
@@ -1,6 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Place(models.Model):
+    name = models.CharField(null=True, max_length=128)
+
+    class Meta:
+        db_table = 'places'
+
+
+class Category(models.Model):
+    name = models.CharField(null=True, max_length=128)
+
+    class Meta:
+        db_table = 'categories'
+
+
+class Tag(models.Model):
+    name = models.CharField(null=True, max_length=128)
+    weight = models.IntegerField(default=1)
+
+    class Meta:
+        db_table = 'tags'
 
 
 class ImageFile(models.Model):
@@ -27,52 +47,9 @@ class ImageData(models.Model):
     rating = models.IntegerField(null=True)
     creative = models.BooleanField(default=False)
     is_publish = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag, null=True, blank=True)
+    categories = models.ManyToManyField(Category, null=True, blank=True)
+    place = models.ForeignKey(Place, null=True, blank=True, on_delete=models.SET_NULL)
 
     class Meta:
         db_table = 'image_data'
-
-
-class Place(models.Model):
-    name = models.CharField(null=True, max_length=128)
-
-    class Meta:
-        db_table = 'places'
-
-
-class Category(models.Model):
-    name = models.CharField(null=True, max_length=128)
-
-    class Meta:
-        db_table = 'categories'
-
-
-class Tag(models.Model):
-    name = models.CharField(null=True, max_length=128)
-    weight = models.IntegerField(default=1)
-
-    class Meta:
-        db_table = 'tags'
-
-
-class ImageToPlace(models.Model):
-    image = models.ForeignKey(ImageFile, on_delete=models.CASCADE)
-    place = models.ForeignKey(Place, on_delete=models.CASCADE)
-
-    class Meta:
-        db_table = 'images_to_places'
-
-
-class ImageToCategory(models.Model):
-    image = models.ForeignKey(ImageFile, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-
-    class Meta:
-        db_table = 'images_to_categories'
-
-
-class ImageToTag(models.Model):
-    image = models.ForeignKey(ImageFile, on_delete=models.CASCADE)
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
-
-    class Meta:
-        db_table = 'images_to_tags'
